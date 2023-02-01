43692978c517abbcb0557f5380379fe8f9d82480
Bexultan <mustafin.beksultan04@gmail.com>
2023-02-02 03:55:30 +0600

    Laboratory 2

--- Laboratory 2/Lists/Example/Copy Lists/Example 2.py
+++ Laboratory 2/Lists/Example/Copy Lists/Example 2.py
@@ -1,3 +1,22 @@
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
+n = int(input())
+a = []
+requests = []
+isTrueRequests = False
+for i in range(n):
+    l = input()
+    a.append(l)
+k = int(input())
+for i in range(k):
+    requests.append(input())
+result = []
+for i in range(n):
+    for j in range(k):
+        if a[i].lower().find(requests[j].lower()) != -1:
+            isTrueRequests = True
+        else:
+            isTrueRequests = False
+            break
+    if isTrueRequests == True:
+        result.append(a[i])
+        isTrueRequests = False
+print(*result, sep='\n')
